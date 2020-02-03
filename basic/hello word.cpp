664bbe94818329be1a2c567230f537b7c7bdfa51
Monicaofak <56330821+Monicaofak@users.noreply.github.com>
2020-02-03 18:06:48 +0800

    Create hello word.cpp

---
+++ basic/hello word.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h> //以#开头的语句称为预处理器指令
+int main()    //#include 语句不是必须的额，但是如果一旦程序中有该语句，就必须放在开始处
+ {            //stuio 标准输入输出standared input output .h:header头，头文件
+ printf("hello word!");//main:C程序处理的起点/入口，可以返回一个值，也可以不返回值:关键字
+                         //void
+ return 0;               //printf:在屏幕上打印一行文字，分号表示语句结束 \n换行
+ }
